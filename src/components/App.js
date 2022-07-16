2eb6bff816c7e912e5c080ad8cceb3bc451cd92c
ABHIJITRAUT123 <106418992+ABHIJITRAUT123@users.noreply.github.com>
2022-07-16 12:57:51 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,12 @@ import "./../styles/App.css";
 
 function App() {
   return (
-    <div id="main">
-      // Do not alter the main div
-    </div>
+    <>
+      <div id="main">
+         <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+      </div>
+      <button id="click">Click</button>
+    </>
   );
 }
 
